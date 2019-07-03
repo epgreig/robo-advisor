7b98553235f3ae1bea065794a98aa375ec7b1530
ruse <razvanil@gmail.com>
2019-07-03 14:23:36 -0400

    Add skeleton for quarterly report

--- excel.py
+++ excel.py
@@ -6,10 +6,17 @@ wb.remove(wb.active)
 composition = wb.create_sheet("Composition", 0)
 composition.sheet_properties.tabColor = "DC143C"
 
-analytics = wb.create_sheet("Risk Analytics", 1)
-analytics.sheet_properties.tabColor = "FFD700"
-
 analytics = wb.create_sheet("Performance", 2)
 analytics.sheet_properties.tabColor = "228B22"
 
+analytics = wb.create_sheet("Risk Analytics", 1)
+analytics.sheet_properties.tabColor = "FFD700"
+
 wb.save("Report.xlsx")
+
+# Three worksheets for each type of analytic:
+#
+# Composition
+# Backward Peformance Analytics
+# Forward Risk Analytics
+
