418a09c81aaae2f7f51e7d52a06ac44f1914f6bf
sunnytung123 <zttung_94@hotmail.com>
2019-07-02 23:44:37 -0400

    Update risk_parity.py
    added risk parity by asset class method

--- risk_parity.py
+++ risk_parity.py
@@ -14,6 +14,7 @@ class RiskParity:
         self.hist = hist
         env_df = pd.read_csv("data/Consolidated.csv", header = [0,1], index_col=[0])
         self.etfs = env_df[['EQ', 'FI', 'EM', 'RE']].columns.droplevel()
+        self.envdf = env_df
         
     def get_weights(self, date: Timestamp):
         self.date = date
@@ -29,9 +30,52 @@ class RiskParity:
         
         non_neg_constr = {'type':'ineq', 'fun':lambda x: np.dot(np.identity(N), x), 'jac':lambda x: np.identity(N)}
         y = optimize.minimize(f, y0, jac=grad_f, constraints=non_neg_constr, tol=1e-7, options={'maxiter':500})
-        print((y.nit, y.message))
+        # print((y.nit, y.message))
         wts = y.x/sum(y.x)
         self.weights_vector = wts
         self.weights_dict = {self.etfs[i]:wts[i] for i in range(N)}
+        
+        return self.weights_dict
+    
+    def get_weights_ac(self, date: Timestamp):
+        self.date = date
+        
+        asset_classes = ['EQ', 'FI', 'EM', 'RE']
+        self.mutest = []
+        for asset in asset_classes:
+            etfs_inclass = self.envdf[[asset]].columns.droplevel()
+            self.mutest.append(np.mean(ShockMap(self.hist, self.date).targets_df[etfs_inclass], axis = 1))
+        
+        self.mu = pd.DataFrame(self.mutest[0])
+        for i in range(1, 4):
+            self.mu[i] = self.mutest[i]
+        self.mu.columns = asset_classes
+        
+        
+        self.Q = np.array(self.mu.cov())
+        N = len(asset_classes)
+        y0 = np.ones(N)/N
+
+        def f(y):
+            return 0.5*np.dot(y, np.dot(self.Q, y)) - 0.1*sum(np.log(y))
+        
+        def grad_f(y):
+            return np.dot(self.Q, y) - 0.1/y
+        
+        non_neg_constr = {'type':'ineq', 'fun':lambda x: np.dot(np.identity(N), x), 'jac':lambda x: np.identity(N)}
+        y = optimize.minimize(f, y0, jac=grad_f, constraints=non_neg_constr, tol=1e-7, options={'maxiter':500})
+        print((y.nit, y.message))
+        wts = y.x/sum(y.x)
+        self.weights_vector = wts
+        self.weights_dict = {}
+        
+        for i in range(0,4):
+            etfs_inclass = self.envdf[[asset_classes[i]]].columns.droplevel()
+            num_etfs = len(etfs_inclass)
+            inclass_weight = wts[i]/num_etfs
+            for etf in etfs_inclass:
+                self.weights_dict[etf] = inclass_weight
+        
+        #self.weights_dict = {self.etfs[i]:wts[i] for i in range(N)}
         
         return self.weights_dict
